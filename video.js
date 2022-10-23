bef8ae4c156ebb3039ccbdc0bf3c6fd4ba6f927f
Dani <96921848+JLIHess@users.noreply.github.com>
2022-10-23 19:21:54 -0400

    Video Page complete

--- video.js
+++ video.js
@@ -3,15 +3,15 @@ const container = document.querySelector(".container"),
     videoTimeline = container.querySelector(".video-timeline"),
     progressBar = container.querySelector(".progress-bar"),
     volumeBtn = container.querySelector(".volume i"),
-    volumeSlider = container.querySelector(".left input");
-currentVidTime = container.querySelector(".current-time"),
+    volumeSlider = container.querySelector(".left input"),
+    currentVidTime = container.querySelector(".current-time"),
     videoDuration = container.querySelector(".video-duration"),
     skipBackward = container.querySelector(".skip-backward i"),
     skipForward = container.querySelector(".skip-forward i"),
     playPauseBtn = container.querySelector(".play-pause i"),
     speedBtn = container.querySelector(".playback-speed span"),
     speedOptions = container.querySelector(".speed-options"),
-    pipBtn = container.querySelector(".pic-in-pic span"),
+    // pipBtn = container.querySelector(".pic-in-pic span"),
     fullScreenBtn = container.querySelector(".fullscreen i");
 let timer;
 const hideControls = () => {
@@ -105,7 +105,7 @@ fullScreenBtn.addEventListener("click", () => {
     container.requestFullscreen();
 });
 speedBtn.addEventListener("click", () => speedOptions.classList.toggle("show"));
-pipBtn.addEventListener("click", () => mainVideo.requestPictureInPicture());
+// pipBtn.addEventListener("click", () => mainVideo.requestPictureInPicture());
 skipBackward.addEventListener("click", () => mainVideo.currentTime -= 5);
 skipForward.addEventListener("click", () => mainVideo.currentTime += 5);
 mainVideo.addEventListener("play", () => playPauseBtn.classList.replace("fa-play", "fa-pause"));

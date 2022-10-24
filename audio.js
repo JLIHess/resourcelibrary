ff20cac2e3162f4c9da7086131860377bc677af7
Dani <96921848+JLIHess@users.noreply.github.com>
2022-10-23 23:44:05 -0400

    Audio Page

--- audio.js
+++ audio.js
@@ -1,162 +1,122 @@
-let now_playing = document.querySelector('.now-playing');
-let track_art = document.querySelector('.track-art');
-let track_name = document.querySelector('.track-name');
-let track_artist = document.querySelector('.track-artist');
 
-let playpause_btn = document.querySelector('.playpause-track');
-let next_btn = document.querySelector('.next-track');
-let prev_btn = document.querySelector('.prev-track');
+// let now_playing = document.querySelector(".now-playing");
+// let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+// let track_artist = document.querySelector(".track-artist");
 
-let seek_slider = document.querySelector('.seek_slider');
-let volume_slider = document.querySelector('.volume_slider');
-let curr_time = document.querySelector('.current-time');
-let total_duration = document.querySelector('.total-duration');
-let wave = document.getElementById('wave');
-let randomIcon = document.querySelector('.fa-random');
-let curr_track = document.createElement('audio');
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
 
 let track_index = 0;
 let isPlaying = false;
-let isRandom = false;
 let updateTimer;
 
-const music_list = [
-    // {
-    //     img: 'images/stay.png',
-    //     name: 'Niggun',
-    //     artist: 'Chabad',
-    //     music: 'music/stay.mp3'
-    // }
-    // },
-    // {
-    //     img: 'images/fallingdown.jpg',
-    //     name: 'Falling Down',
-    //     artist: 'Wid Cards',
-    //     music: 'music/fallingdown.mp3'
-    // },
-    // {
-    //     img: 'images/faded.png',
-    //     name: 'Faded',
-    //     artist: 'Alan Walker',
-    //     music: 'music/Faded.mp3'
-    // },
-    // {
-    //     img: 'images/ratherbe.jpg',
-    //     name: 'Rather Be',
-    //     artist: 'Clean Bandit',
-    //     music: 'music/Rather Be.mp3'
-    // }
+// Create new audio element
+let curr_track = document.createElement('audio');
+
+// Define the tracks that have to be played
+let track_list = [
+    {
+        name: "Night Owl",
+        // artist: "Broke For Free",
+        // image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+        path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
+    },
+    {
+        name: "Enthusiast",
+        // artist: "Tours",
+        // image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+        path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
+    },
+    {
+        name: "Shipping Lanes",
+        // artist: "Chad Crouch",
+        // image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+        path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
+    },
 ];
 
-loadTrack(track_index);
+
 
 function loadTrack(track_index) {
     clearInterval(updateTimer);
-    reset();
-
-    curr_track.src = music_list[track_index].music;
+    resetValues();
+    curr_track.src = track_list[track_index].path;
     curr_track.load();
 
-    // track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
-    track_name.textContent = music_list[track_index].name;
-    // track_artist.textContent = music_list[track_index].artist;
-    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+    // track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+    track_name.textContent = track_list[track_index].name;
+    // track_artist.textContent = track_list[track_index].artist;
+    // now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
 
-    updateTimer = setInterval(setUpdate, 1000);
-
-    curr_track.addEventListener('ended', nextTrack);
+    updateTimer = setInterval(seekUpdate, 1000);
+    curr_track.addEventListener("ended", nextTrack);
     random_bg_color();
 }
 
-function random_bg_color() {
-    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
-    let a;
-
-    function populate(a) {
-        for (let i = 0; i < 6; i++) {
-            let x = Math.round(Math.random() * 14);
-            let y = hex[x];
-            a += y;
-        }
-        return a;
-    }
-    let Color1 = populate('#');
-    let Color2 = populate('#');
-    var angle = 'to right';
-
-    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
-    document.body.style.background = gradient;
-}
-function reset() {
+function resetValues() {
     curr_time.textContent = "00:00";
     total_duration.textContent = "00:00";
     seek_slider.value = 0;
 }
-function randomTrack() {
-    isRandom ? pauseRandom() : playRandom();
-}
-function playRandom() {
-    isRandom = true;
-    randomIcon.classList.add('randomActive');
-}
-function pauseRandom() {
-    isRandom = false;
-    randomIcon.classList.remove('randomActive');
-}
-function repeatTrack() {
-    let current_index = track_index;
-    loadTrack(current_index);
-    playTrack();
-}
+
+// Load the first track in the tracklist
+loadTrack(track_index);
+
 function playpauseTrack() {
-    isPlaying ? pauseTrack() : playTrack();
+    if (!isPlaying) playTrack();
+    else pauseTrack();
 }
+
 function playTrack() {
     curr_track.play();
     isPlaying = true;
-    track_art.classList.add('rotate');
-    wave.classList.add('loader');
-    playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+    playpause_btn.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
 }
+
 function pauseTrack() {
     curr_track.pause();
     isPlaying = false;
-    track_art.classList.remove('rotate');
-    wave.classList.remove('loader');
-    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+    playpause_btn.innerHTML = '<i class="fa fa-play fa-2x"></i>';;
 }
+
 function nextTrack() {
-    if (track_index < music_list.length - 1 && isRandom === false) {
+    if (track_index < track_list.length - 1)
         track_index += 1;
-    } else if (track_index < music_list.length - 1 && isRandom === true) {
-        let random_index = Number.parseInt(Math.random() * music_list.length);
-        track_index = random_index;
-    } else {
-        track_index = 0;
-    }
+    else track_index = 0;
     loadTrack(track_index);
     playTrack();
 }
+
 function prevTrack() {
-    if (track_index > 0) {
+    if (track_index > 0)
         track_index -= 1;
-    } else {
-        track_index = music_list.length - 1;
-    }
+    else track_index = track_list.length;
     loadTrack(track_index);
     playTrack();
 }
+
 function seekTo() {
     let seekto = curr_track.duration * (seek_slider.value / 100);
     curr_track.currentTime = seekto;
 }
+
 function setVolume() {
     curr_track.volume = volume_slider.value / 100;
 }
-function setUpdate() {
+
+function seekUpdate() {
     let seekPosition = 0;
+
     if (!isNaN(curr_track.duration)) {
         seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+
         seek_slider.value = seekPosition;
 
         let currentMinutes = Math.floor(curr_track.currentTime / 60);
@@ -170,6 +130,6 @@ function setUpdate() {
         if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
 
         curr_time.textContent = currentMinutes + ":" + currentSeconds;
-        total_duration.textContent = durationMinutes + ":" + durationMinutes;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
     }
 }
